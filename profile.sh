b17e8addd8088d4ff47c43062dcc38a0c7e7bb60
GeistInDerSH <GeistInDerSH@gmail.com>
2024-03-27 19:35:16 -0700

    Add a script for PGO
    Add a script to compile the program and use profiling guided optimization to try and generate a faster program

---
+++ profile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# STEP 0: Make sure there is no left-over profiling data from previous runs
+rm -rf /tmp/pgo-data
+
+# STEP 1: Build the instrumented binaries
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" \
+    cargo build --release --target=x86_64-unknown-linux-gnu
+
+# STEP 2: Run the instrumented binaries with some typical data
+for i in {0..3}; do
+  ./target/x86_64-unknown-linux-gnu/release/rust_1brr > /dev/null
+done
+
+# STEP 3: Merge the `.profraw` files into a `.profdata` file
+llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
+
+# STEP 4: Use the `.profdata` file for guiding optimizations
+RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata" \
+    cargo build --release --target=x86_64-unknown-linux-gnu
